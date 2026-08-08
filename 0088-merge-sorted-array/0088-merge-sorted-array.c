1a63277adc228f941f0966a8179e5a7238836daa
rhythmsingh2709 <rhythmsingh2709@gmail.com>
2026-08-08 11:46:01 +0530

    Time: 0 ms (100%), Space: 10.6 MB (94.59%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -0,0 +1,25 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int i = m - 1; // Last element of valid nums1
+    int j = n - 1; // Last element of nums2
+    int k = m + n - 1; // Last position of nums1
+
+    // Traverse both arrays and place the larger element at the end of nums1
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // If there are remaining elements in nums2, copy them
+    // (No need to copy remaining nums1 elements as they are already in place)
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
